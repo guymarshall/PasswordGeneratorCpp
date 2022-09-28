f9baaed7852dbf23fa6e71e49eb664c4f1b78533
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-09-28 12:27:51 +0100

    Move main() to below generatePassword()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char const *argv[])
-{
-    return 0;
-}
-
 std::string generatePassword(int length)
 {
     char characters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '\'', '£', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', '@', '#', '~', ',', '<', '.', '>', '/', '?'};
@@ -26,6 +21,11 @@ std::string generatePassword(int length)
     }
 
     return password;
+}
+
+int main(int argc, char const *argv[])
+{
+    return 0;
 }
 
 // public static void main(String[] args) {
