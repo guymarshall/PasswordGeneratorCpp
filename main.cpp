5c4a08e463ed6b800744a2ff1983ba552e3bb41d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-09-29 12:12:07 +0100

    Remove completed commented code

--- main.cpp
+++ main.cpp
@@ -38,24 +38,12 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-// public static void main(String[] args) {
-//     System.out.print("Enter number of characters: ");
-//     Scanner scanner = new Scanner(System.in);
-//     int numberOfCharacters = scanner.nextInt();
-
-//     final String[] characters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "£", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ";", ":", "'", "@", "#", "~", ",", "<", ".", ">", "/", "?"};
-
-//     System.out.println("Generating random password...");
-//     final String password = generatePassword(characters, numberOfCharacters);
-
-//     System.out.println("Saving password to file");
-//     try {
-//         FileWriter myWriter = new FileWriter("password.txt");
-//         myWriter.write(password);
-//         myWriter.close();
-//         System.out.println("Password has been saved to file.");
-//     } catch (IOException e) {
-//         System.out.println("An error occurred.");
-//         e.printStackTrace();
-//     }
+// try {
+//     FileWriter myWriter = new FileWriter("password.txt");
+//     myWriter.write(password);
+//     myWriter.close();
+//     System.out.println("Password has been saved to file.");
+// } catch (IOException e) {
+//     System.out.println("An error occurred.");
+//     e.printStackTrace();
 // }
